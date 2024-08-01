4963bc03b81c6cd16b045944f0c28ec2a368fee4
Suseendran12 <suseendranss12@gmail.com>
2024-08-01 19:49:09 +0530

    reverting changes in settings.py

--- check/settings.py
+++ check/settings.py
@@ -82,13 +82,8 @@ WSGI_APPLICATION = 'check.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'URL': 'postgresql://postgres:iZCvczYoTUMgLirwxyGCaljHNTLYBQsz@postgres.railway.internal:5432/railway',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'iZCvczYoTUMgLirwxyGCaljHNTLYBQsz',
-        'HOST': 'postgres.railway.internal',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
